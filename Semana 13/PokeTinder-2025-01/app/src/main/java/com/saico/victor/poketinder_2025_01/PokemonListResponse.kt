1636bce00e7cb50978efd07c9fbe98f1670f3711
Hector-Perez-Vengoa <hector.perez@tecsup.edu.pe>
2025-07-05 23:48:37 -0500

    correcion

--- Semana 13/PokeTinder-2025-01/app/src/main/java/com/saico/victor/poketinder_2025_01/PokemonListResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.saico.victor.poketinder_2025_01
-
-data class PokemonListResponse(
-    val count: Int,
-    val next: String,
-    val results: List<PokemonResponse>
-)
--- eval4/PokeTinder-main/PokeTinder-main/app/src/main/java/com/garcia/rafael/poketinder/PokemonListResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.garcia.rafael.poketinder
-
-data class PokemonListResponse(
-    val count: Int,
-    val next: String,
-    val results: List<PokemonResponse>
-)
